988dfea8cc6649afba8703a1f3a95aa0bb0fbdb4
Alsahir <sudarkin.jan@yandex.ru>
2023-10-26 12:42:37 +0400

    Done

---
+++ telegram-bot/target/classes/liquibase/scripts/01_scripts.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+
+-- changeset ysudarkin:create_notification_tasks
+CREATE TABLE notification_tasks
+(
+    id SERIAL,
+    message TEXT NOT NULL,
+    chat_id BIGINT NOT NULL,
+    notification_data_time TIMESTAMP NOT NULL
+);
+
